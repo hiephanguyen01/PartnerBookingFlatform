2dceccdd4cb0c59881fcba62fd307d89a454d894
cCyberBear <duycazami92012001@gmail.com>
2023-08-10 17:08:57 +0700

    page

--- app/home/support/page.js
+++ app/home/support/page.js
@@ -1,11 +1,39 @@
-import React from "react";
+import { SUPPORT_SECTION_LIST } from "@/assets/templist";
+import SupportCard from "@/components/SupportCard";
+import { Breadcrumb, Col, Row } from "antd";
+import classes from "./support.module.scss";
 
-const index = () => {
+const Support = () => {
   return (
-    <main style={{ height: "500px", backgroundColor: "#ccc" }}>
-      <div className="container">Support Page</div>
+    <main style={{ minHeight: "100vh" }} className={classes.support}>
+      <div className={classes.head}>
+        <h1>Bạn cần hỗ trợ về vấn đề gì?</h1>
+      </div>
+      <div className="container" style={{ paddingBottom: "40px" }}>
+        <div style={{ margin: "40px 0" }}>
+          <Breadcrumb
+            separator=">"
+            items={[
+              {
+                title: "Trang chủ",
+                href: "/home",
+              },
+              {
+                title: "Hỗ trợ",
+              },
+            ]}
+          />
+        </div>
+        <Row gutter={[28, 28]}>
+          {SUPPORT_SECTION_LIST.map((item) => (
+            <Col lg={8} md={12} sm={24}>
+              <SupportCard item={item} key={item.id} />
+            </Col>
+          ))}
+        </Row>
+      </div>
     </main>
   );
 };
 
-export default index;
+export default Support;
--- app/home/trend/page.js
+++ app/home/trend/page.js
@@ -1,11 +1,39 @@
-import React from "react";
+import { SUPPORT_SECTION_LIST } from "@/assets/templist";
+import TrendCard from "@/components/TrendCard";
+import { Breadcrumb, Col, Row } from "antd";
+import classes from "./trend.module.scss";
 
-const index = () => {
+const Trend = () => {
   return (
-    <main style={{ height: "500px", backgroundColor: "#ccc" }}>
-      <div className="container">Trend Page</div>
+    <main style={{ minHeight: "100vh" }} className={classes.trend}>
+      <div className={classes.head}>
+        <h1>Khám phá xu hướng và tin tức mới nhất</h1>
+      </div>
+      <div className="container" style={{ paddingBottom: "40px" }}>
+        <div style={{ margin: "40px 0" }}>
+          <Breadcrumb
+            separator=">"
+            items={[
+              {
+                title: "Trang chủ",
+                href: "/home",
+              },
+              {
+                title: "Xu hướng, tin tức",
+              },
+            ]}
+          />
+        </div>
+        <Row gutter={[28, 28]}>
+          {SUPPORT_SECTION_LIST.map((item) => (
+            <Col lg={12} md={12} sm={24}>
+              <TrendCard item={item} key={item.id} />
+            </Col>
+          ))}
+        </Row>
+      </div>
     </main>
   );
 };
 
-export default index;
+export default Trend;
