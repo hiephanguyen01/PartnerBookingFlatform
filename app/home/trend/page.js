d3c61918e4578fd70542af5cf31fde75749b0c00
cCyberBear <duycazami92012001@gmail.com>
2023-08-14 15:45:56 +0700

    push

--- app/home/trend/page.js
+++ app/home/trend/page.js
@@ -1,9 +1,61 @@
-import { SUPPORT_SECTION_LIST } from "@/assets/templist";
+"use client";
+import React, { useEffect, useState } from "react";
+import { Breadcrumb, Col, Row, Tabs, Pagination } from "antd";
 import TrendCard from "@/components/TrendCard";
-import { Breadcrumb, Col, Row } from "antd";
+import { CATEGORIES } from "@/utils/category";
 import classes from "./trend.module.scss";
+import { partnerHubTrendNewsService } from "@/services/PartnerHubTrendNewsService";
 
 const Trend = () => {
+  const [data, setData] = useState([]);
+  const [dataFilled, setDataFilled] = useState([]);
+  const [totalItemsFilled, setTotalItemsFilled] = useState(0);
+
+  const [currentTab, setCurrentTab] = useState(1);
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 6;
+
+  // Handle tab change
+  const handleTabChange = (tabKey) => {
+    setCurrentTab(tabKey);
+    setCurrentPage(1);
+  };
+
+  // Handle page change
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+  };
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const { data: res } =
+          await partnerHubTrendNewsService.getPartnerHubTrendNews();
+        const regex = /(<([^>]+)>)/gi;
+
+        setData(
+          res.data.map((val) => ({
+            ...val,
+            content: val.content.replace(regex, "").replaceAll("&nbsp;", " "),
+          }))
+        );
+      } catch (error) {
+        console.log("🚀 ~ file: page.js:17 ~ error:", error);
+      }
+    })();
+  }, []);
+
+  useEffect(() => {
+    const filteredItems = data.filter((item) => item.category === currentTab);
+    const totalItems = filteredItems.length;
+    setTotalItemsFilled(totalItems);
+
+    const startIndex = (currentPage - 1) * itemsPerPage;
+    const endIndex = startIndex + itemsPerPage;
+    const itemsToShow = filteredItems.slice(startIndex, endIndex);
+    setDataFilled(itemsToShow);
+  }, [currentTab, currentPage, data]);
+
   return (
     <main style={{ minHeight: "100vh" }} className={classes.trend}>
       <div className={classes.head}>
@@ -24,13 +76,32 @@ const Trend = () => {
             ]}
           />
         </div>
-        <Row gutter={[28, 28]}>
-          {SUPPORT_SECTION_LIST.map((item) => (
-            <Col lg={12} md={12} sm={24}>
-              <TrendCard item={item} key={item.id} />
-            </Col>
-          ))}
-        </Row>
+        <Tabs
+          defaultActiveKey={1}
+          style={{ marginBottom: 24 }}
+          items={CATEGORIES}
+          onChange={handleTabChange} // Handle tab change
+        />
+        {dataFilled.length > 0 ? (
+          <>
+            <Row gutter={[28, 28]}>
+              {dataFilled.map((item) => (
+                <Col lg={12} md={12} sm={24} key={item.id}>
+                  <TrendCard item={item} />
+                </Col>
+              ))}
+            </Row>
+            <Pagination
+              current={currentPage}
+              pageSize={itemsPerPage}
+              total={totalItemsFilled}
+              onChange={handlePageChange} // Handle page change
+              style={{ marginTop: 24, textAlign: "center" }}
+            />
+          </>
+        ) : (
+          <>Hiện đang trống</>
+        )}
       </div>
     </main>
   );
--- components/SupportCard/index.js
+++ components/SupportCard/index.js
@@ -1,13 +1,21 @@
 "use client";
 import React from "react";
 import classes from "./supportcard.module.scss";
+import { useRouter } from "next/navigation";
+import { Image } from "antd";
 
 const SupportCard = ({ item }) => {
+  const router = useRouter();
   return (
-    <div className={classes.supportSectionItem}>
-      <div className={classes.icon}>{item.icon}</div>
+    <div
+      className={classes.supportSectionItem}
+      onClick={() => router.push(`/home/support/${item.id}`)}
+    >
+      <div className={classes.icon}>
+        <Image src={item.image} preview={false} />
+      </div>
       <label>{item.title}</label>
-      <p>{item.description}</p>
+      <p className={classes.content}>{item.content}</p>
     </div>
   );
 };
