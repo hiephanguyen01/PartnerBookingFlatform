9b616cdde54dff67c27fa66db101226a8b18f3d7
cCyberBear <75051983+cCyberBear@users.noreply.github.com>
2023-10-12 09:27:25 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,8 +5,8 @@ module.exports = (phase, { defaultConfig }) => {
   const homePath = process.env.NEXT_PUBLIC_HOME_PATH || "/home";
   return {
     env: {
-      BASE_URL: isProduction ? process.env.BASE_URL : process.env.BASE_URL,
-      BASE_URL_IMAGE:process.env.BASE_URL_IMAGE
+      BASE_URL: isProduction ? "https://am.bookingstudio.vn" : process.env.BASE_URL,
+      BASE_URL_IMAGE: "https://am.bookingstudio.vn/api/image""
     },
     async redirects() {
       return [
